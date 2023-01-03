3a59af783f8aedfe411b3abcfce109278f9d7342
narutooxox <112558957+narutooxox@users.noreply.github.com>
2023-01-03 23:10:02 +0530

    I Wanna Be Yours! ❤

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 
      # Auto Delete For Group Message (Self Delete) #
-SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 600))
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 10800))
 SELF_DELETE = environ.get('SELF_DELETE', True)
 if SELF_DELETE == "True":
     SELF_DELETE = True
